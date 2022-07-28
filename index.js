16239f6576cb1bae3d5cbb092b9f9731f90167a9
Ana Olivero <anagoliverom.2002@gmail.com>
2022-07-27 22:12:37 -0400

    Algoritmo con un ciclo

---
+++ index.js
@@ -0,0 +1,7 @@
+/Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada/
+
+let n = (prompt("ingrese un numero"))
+
+for(let i = 0; i<n; i++){
+    alert("Hola", n);
+}
